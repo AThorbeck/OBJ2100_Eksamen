60805b990c55ca7a38fe516f08735e2b2405f510
AThorbeck <92173193+AThorbeck@users.noreply.github.com>
2023-05-30 12:46:39 +0200

    Add files via upload

---
+++ EksamenMai/src/main/java/Group7/EksamenMai/App.java
@@ -0,0 +1,13 @@
+package Group7.EksamenMai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
