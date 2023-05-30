b2e2148f98bc03beceab7eb3208e53b605653765
HermanASolberg <69518762+HermanASolberg@users.noreply.github.com>
2023-05-30 15:33:59 +0200

    Create JDBC.java

---
+++ obj2100/src/main/java/group7/obj2100/JDBC.java
@@ -0,0 +1,43 @@
+package group7.obj2100;
+import java.sql.*;
+
+//@author www.luv2code.com
+
+public class JDBCDemo {
+public static void main(String[] args) throws SQLException {
+
+    Connection myConn = null;
+    Statement myStmt = null;
+    ResultSet myRs = null;
+
+    String dbUrl = "jdbc:mysql://localhost:3306/classicmodels";
+    String user = "root";
+    String pass = "";
+    try {
+        // 1. Get a connection to database
+        myConn = DriverManager.getConnection(dbUrl, user, pass);
+        System.out.println("Database successfully connected");
+
+
+        // 2. Create a statement
+        myStmt = myConn.createStatement();
+
+        myRs = myStmt.executeQuery("SELECT * FROM offices ");
+
+        while (myRs.next()) {
+        System.out.println(myRs.getString("city") + ", " + myRs.getString("country"));
+        }
+    }
+
+        finally{
+        if(myRs != null){
+        myRs.close();}
+
+        if (myStmt != null) {
+            myStmt.close();}
+
+        if (myConn != null){
+        myConn.close();}
+    }
+}
+}
