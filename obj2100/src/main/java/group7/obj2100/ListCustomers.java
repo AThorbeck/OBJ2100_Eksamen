48fb473c41563fe79578e93c2b7626c097f93cc0
Martulv <123483286+Martulv@users.noreply.github.com>
2023-06-01 17:56:31 +0200

    Update ListCustomers.java
    Oppdaterte filen

--- obj2100/src/main/java/group7/obj2100/ListCustomers.java
+++ obj2100/src/main/java/group7/obj2100/ListCustomers.java
@@ -1,15 +1,46 @@
 /*
-import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import javax.swing.*;
 
-public class ListCustomers extends JPanel
+public class ListCustomers 
 {
 	static String cityString = "City";
-	static String stateString = "State";	
+	static String stateString = "State";
 	
-	public static void main(String[] args)
-	{	
+	static String url = "jdbc:mysql://localhost:3306/classicmodels";
+	static String user = "root";
+	static String pass = "";
+	
+	public static void main(String[] args) 
+	{		
+		List cityData = new ArrayList();
+		List stateData = new ArrayList();
+
+		try 
+		{
+	    Connection con = null;
+		con = DriverManager.getConnection(url, user, pass);
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("Select * FROM customers");
+		}
+		
+		while (rs.next()) 
+		{
+			String dbCity = rs.getString("city");
+			String dbState = rs.getString("state");
+		} 
+		catch (Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+		
+		private static void jframe()
+		{
 		JFrame frame = new JFrame("List Customers");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -18,37 +49,73 @@ public class ListCustomers extends JPanel
 		cityRadio.setActionCommand(cityString);
 		cityRadio.setSelected(true);
 		
-		cityRadio.addActionListener(new ActionListener("City Radio")
+		cityRadio.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				if (cityRadio.isSelected()) 
 				{
-					public void actionPerformed(ActionEvent e) 
-					{
-						if (cityRadio.isSelected()) 
-						{
-							String[] data = {"Norway", "Sweden", "Denmark"};
-							JList<String> cityList = new JList<String>(data);
-						}
-					}
+					String[] cityData = {dbCity};
+					JList<String> cityList = new JList<String>(cityData);
 				}
+			}
+		});
 		
 		JRadioButton stateRadio = new JRadioButton("By State");
 		stateRadio.setMnemonic(KeyEvent.VK_B);
 		stateRadio.setActionCommand(stateString);
 		
-		stateRadio.addActionListener(new ActionListener("State Radio")
+		stateRadio.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
 				if (stateRadio.isSelected()) 
 				{
-					String[] data = {"California", "Florida", "New York"};
-					JList<String> stateList = new JList<String>(data);
+					String[] stateData = {dbState};
+					JList<String> stateList = new JList<String>(stateData);
 				}
 			}
-		}
+		});
 		
-		JButton write = new JButton("Write customer list based on selected criteria");
-		
-		
-	}	
+		JButton writeToFile = new JButton("Write to file");
+		writeToFile.setBounds(50,200,95,30);
+		writeToFile.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				if (cityRadio.isSelected())
+				{
+					try
+					{
+						Statement stCity = con.createStatement();
+						ResultSet rsCity = myst.executeQuery("Select * FROM customers WHERE city = cityData");
+						
+						FileWriter myWriter = new FileWriter(Filename);			// endre når du vet hva filnavnet er 
+						myWriter.write(rsCity);				// Skal skrive de kundene fra den selecta byen
+						myWriter.close();
+						System.out.println("Successfully wrote to the file");
+					}
+				}
+				if (stateRadio.isSelected())
+				{
+					try
+					{
+						Statement stState = con.createStatement();
+						ResultSet rsState = myst.executeQuery("Select * FROM customers WHERE state = stateData");
+						
+						FileWriter myWriter = new FileWriter(Filename);
+						myWriter.write(rsState);				// Skal skrive de kundene fra den selecta staten
+						myWriter.close();
+						System.out.println("Successfully wrote to the file");
+					}
+				}
+				catch (IOException e) 
+				{
+					System.out.println("An error has occurred");
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
 */
