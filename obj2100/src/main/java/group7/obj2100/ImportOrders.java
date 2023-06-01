ee90579ad7c2c057cf416c14940e9ea7eaac19fe
02Arman <123482732+02Arman@users.noreply.github.com>
2023-06-01 13:53:39 +0200

    ImportOrders.java

---
+++ obj2100/src/main/java/group7/obj2100/ImportOrders.java
@@ -0,0 +1,29 @@
+package group7.obj2100;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImportOrders {
+
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel("Here is bulk import of orders:"); // title of page
+
+
+    ImportOrders() {
+
+
+        label.setFont(new Font(null, Font.PLAIN,20));
+        label.setBounds(70,30,2000,140);
+
+        frame.add(label);
+
+
+        frame.setTitle("Bulk import of orders");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+    }
+}
