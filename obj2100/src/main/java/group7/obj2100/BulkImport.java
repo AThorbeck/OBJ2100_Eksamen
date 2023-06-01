94b2c0a5c072a0e727283a9ba881d7238c0da50e
ChristofferOseberg <247039@usn.no>
2023-06-01 20:54:42 +0200

    Update BulkImport.java
    Beutified the code a little bit.

--- obj2100/src/main/java/group7/obj2100/BulkImport.java
+++ obj2100/src/main/java/group7/obj2100/BulkImport.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+// This is the main class in the program, which is called BulkImport
 public class BulkImport {
     public static void importFileToDatabase(Connection connection, File file, String tableName) throws IOException, SQLException {
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -19,6 +20,7 @@ public class BulkImport {
                 if (data.length == 13) {
                     String query = "";
 
+                    // If statements, if it is for example customers table that is chosen, it will look for the correct tables that are in database.
                     if (tableName.equals("customers")) {
                         query = "INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
                     } else if (tableName.equals("employees")) {
@@ -36,12 +38,13 @@ public class BulkImport {
                     } else if (tableName.equals("products")) {
                         query = "INSERT INTO products (productCode, productName, productLine, productScale, productVendor, productDescription, quantityInStock, buyPrice, MSRP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
                     }
-
+                    
                     try (PreparedStatement statement = connection.prepareStatement(query)) {
                         for (int i = 0; i < data.length; i++) {
                             statement.setString(i + 1, data[i].trim());
                         }
-
+                        
+                        // Execution of query
                         statement.executeUpdate();
                     }
                 }
@@ -50,7 +53,7 @@ public class BulkImport {
     }
 
 
-
+    // The import method, which allows us to import the file into the database.
     public static void importBulk() {
         JFileChooser fileChooser = new JFileChooser();
         int result = fileChooser.showOpenDialog(null);
@@ -58,18 +61,28 @@ public class BulkImport {
         if (result == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
 
-            String[] tableOptions = {"customers", "employees", "offices", "orderdetails", "orders", "payments", "productlines", "products"};
+            String[] tableOptions = {
+                "customers",
+                "employees",
+                "offices",
+                "orderdetails",
+                "orders",
+                "payments",
+                "productlines",
+                "products"
+            };
             String selectedTable = (String) JOptionPane.showInputDialog(null, "Select the table to import the data into:", "Table Selection", JOptionPane.QUESTION_MESSAGE, null, tableOptions, tableOptions[0]);
-
+            
+            // Database connection
             try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "")) {
                 importFileToDatabase(connection, file, selectedTable);
-                JOptionPane.showMessageDialog(null, "File imported successfully");
+                JOptionPane.showMessageDialog(null, "File imported successfully"); // Displays an message to the user if the import was successful
             } catch (IOException | SQLException e) {
-                JOptionPane.showMessageDialog(null, "Error occurred during import: " + e.getMessage());
+                JOptionPane.showMessageDialog(null, "Error occurred during import: " + e.getMessage()); // Displays error code if something is wrong, or the user has chosen wrong table to import the data into.
             }
         }
     }
-
+    // This makes the program runnable even without the main app
     public static void runBulk() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -78,6 +91,5 @@ public class BulkImport {
         });
     }
 
-    public void importFileToDatabase() {
-    }
+    public void importFileToDatabase() {}
 }
