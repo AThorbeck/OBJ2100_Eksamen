266507fab0751a35293ac5b15c76eef66cb0440f
ChristofferOseberg <247039@usn.no>
2023-06-01 16:03:14 +0200

    Add files via upload
    Bulk import of data to database. It reads the files, and import the files to the correct table with the data in it.

---
+++ obj2100/src/main/java/group7/obj2100/bulkImport .java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.sql.*;
+import javax.swing.*;
+
+public class BulkImport {
+    public static void importFileToDatabase(Connection connection, File file, String tableName) throws IOException, SQLException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 13) {
+                    String query = "";
+
+                    if (tableName.equals("customers")) {
+                        query = "INSERT INTO customers (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    } else if (tableName.equals("employees")) {
+                        query = "INSERT INTO employees (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                    } else if (tableName.equals("offices")) {
+                        query = "INSERT INTO offices (officeCode, city, phone, addressLine1, addressLine2, state, country, postalCode, territory) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    } else if (tableName.equals("orderdetails")) {
+                        query = "INSERT INTO orderdetails (orderNumber, productCode, quantityOrdered, priceEach, orderLineNumber) VALUES (?, ?, ?, ?, ?)";
+                    } else if (tableName.equals("orders")) {
+                        query = "INSERT INTO orders (orderNumber, orderDate, requiredDate, status, comments, customerNumber) VALUES (?, ?, ?, ?, ?, ?)";
+                    } else if (tableName.equals("payments")) {
+                        query = "INSERT INTO payments (customerNumber, checkNumber, paymentDate, amount) VALUES (?, ?, ?, ?)";
+                    } else if (tableName.equals("productlines")) {
+                        query = "INSERT INTO productlines (productLine, textDescription, htmlDescription, image) VALUES (?, ?, ?, ?)";
+                    } else if (tableName.equals("products")) {
+                        query = "INSERT INTO products (productCode, productName, productLine, productScale, productVendor, productDescription, quantityInStock, buyPrice, MSRP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    }
+
+                    try (PreparedStatement statement = connection.prepareStatement(query)) {
+                        for (int i = 0; i < data.length; i++) {
+                            statement.setString(i + 1, data[i].trim());
+                        }
+
+                        statement.executeUpdate();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void importBulk() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+
+            String[] tableOptions = {"customers", "employees", "offices", "orderdetails", "orders", "payments", "productlines", "products"};
+            String selectedTable = (String) JOptionPane.showInputDialog(null, "Select the table to import the data into:", "Table Selection", JOptionPane.QUESTION_MESSAGE, null, tableOptions, tableOptions[0]);
+
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "student", "student")) {
+                importFileToDatabase(connection, file, selectedTable);
+                JOptionPane.showMessageDialog(null, "File imported successfully");
+            } catch (IOException | SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error occurred during import: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                importBulk();
+            }
+        });
+    }
+}
