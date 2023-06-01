b93264068f806aed618d736f362d027274a4cd7b
AThorbeck <92173193+AThorbeck@users.noreply.github.com>
2023-06-01 20:17:00 +0200

    Update AllOffices.java

--- obj2100/src/main/java/group7/obj2100/AllOffices.java
+++ obj2100/src/main/java/group7/obj2100/AllOffices.java
@@ -1,31 +1,51 @@
-package group7.obj2100;
-
+package Group7.EksamenMai;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AllOffices {
+public class AllOffices extends JFrame implements ActionListener {
 
+    JComboBox comboBox;
 
+    AllOffices() {
 
-
-    public static void load() {
-
-        JFrame frame = new JFrame();
-        JLabel label = new JLabel("Here is a list of all offices:");
-
-        label.setFont(new Font(null, Font.PLAIN,20));
-        label.setBounds(70,30,2000,140);
-
-        frame.add(label);
+        this.setLayout(new FlowLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setSize(500, 500);
 
 
-        frame.setTitle("List of all offices");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
-        frame.setLayout(null);
-        frame.setVisible(true);
+
+        String[] offices = {"office1", "office2", "office3"};
+
+        JComboBox comboBox = new JComboBox(offices);
+        comboBox.addActionListener(this);
+
+        setTitle("List of all offices");
+
+        // comboBox.setEditable(true); //search and select
+        // System.out.println(comboBox.getItemCount());
+        // comboBox.addItem("office4"); // adds product
+
+        this.add(comboBox);
+        this.pack();
+        this.setVisible(true);
+
+
+
 
 
     }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()== comboBox) {
+           // System.out.println(comboBox.getSelectedItem());
+            System.out.println(comboBox.getSelectedIndex());
+
+        }
+}
 }
