816d37dd0640d4067074507f5cd23d11779b6311
AThorbeck <92173193+AThorbeck@users.noreply.github.com>
2023-06-01 13:00:43 +0200

    Update EmployeeFormDialog.java
    updates, stores new employees and displays all

--- obj2100/src/main/java/group7/obj2100/EmployeeFormDialog.java
+++ obj2100/src/main/java/group7/obj2100/EmployeeFormDialog.java
@@ -1,3 +1,5 @@
+package Group7.EksamenMai;
+
 import javax.swing.*;
 import java.awt.*;
 import java.sql.Connection;
@@ -6,12 +8,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-//@author Andreas
-
 public class EmployeeFormDialog extends JDialog {
     private JTextField employeeNumberField;
     private JTextField firstNameField;
     private JTextField lastNameField;
+    private JTextField extensionField;
     private JTextField emailField;
     private JTextField officeCodeField;
     private JTextField reportsToField;
@@ -19,8 +20,8 @@ public class EmployeeFormDialog extends JDialog {
 
     // Database connection parameters
     private static String url = "jdbc:mysql://localhost:3306/classicmodels";
-    private static String username = "student";
-    private static String password = "student";
+    private static String username = "root";
+    private static String password = "";
 
     public EmployeeFormDialog(JFrame parent) {
         super(parent, "Employee Form", true);
@@ -34,6 +35,9 @@ public class EmployeeFormDialog extends JDialog {
 
         JLabel firstNameLabel = new JLabel("First Name:");
         firstNameField = new JTextField(20);
+        
+        JLabel extensionLabel = new JLabel("Extension:");
+        extensionField = new JTextField(20);
 
         JLabel emailLabel = new JLabel("Email:");
         emailField = new JTextField(20);
@@ -60,6 +64,8 @@ public class EmployeeFormDialog extends JDialog {
         gbc.gridy++;
         formPanel.add(employeeNumberLabel, gbc);
         gbc.gridy++;
+        formPanel.add(extensionLabel, gbc);
+        gbc.gridy++;
         formPanel.add(emailLabel, gbc);
         gbc.gridy++;
         formPanel.add(officeCodeLabel, gbc);
@@ -79,6 +85,8 @@ public class EmployeeFormDialog extends JDialog {
         gbc.gridy++;
         formPanel.add(employeeNumberField, gbc);
         gbc.gridy++;
+        formPanel.add(extensionField, gbc);
+        gbc.gridy++;
         formPanel.add(emailField, gbc);
         gbc.gridy++;
         formPanel.add(officeCodeField, gbc);
@@ -97,6 +105,14 @@ public class EmployeeFormDialog extends JDialog {
         });
         buttonPanel.add(saveButton);
 
+        JButton updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> {
+            // Update the employee data
+            updateEmployeeData();
+            dispose(); // Close the dialog after updating the record
+        });
+        buttonPanel.add(updateButton);
+
         JButton cancelButton = new JButton("Cancel");
         cancelButton.addActionListener(e -> dispose()); // Close the dialog without saving or modifying
         buttonPanel.add(cancelButton);
@@ -107,100 +123,150 @@ public class EmployeeFormDialog extends JDialog {
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         JButton displayAllButton = new JButton("Display All Employees");
-        displayAllButton.addActionListener(e -> {
-            JDialog displayDialog = new JDialog(this, "All Employees", true);
+        displayAllButton.addActionListener(e -> displayAllEmployees());
 
-            JTextArea employeeTextArea = new JTextArea(10, 30);
-            employeeTextArea.setEditable(false);
-            JScrollPane scrollPane = new JScrollPane(employeeTextArea);
-
-            try {
-                Connection connection = DriverManager.getConnection(url, username, password);
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
-
-                StringBuilder employeesData = new StringBuilder();
-                while (resultSet.next()) {
-                    String empFirstName = resultSet.getString("firstName");
-                    String empLastName = resultSet.getString("lastName");
-                    String empJobTitle = resultSet.getString("jobTitle");
-                    employeesData.append("Employee Data: ").append(empFirstName).append(" ").append(empLastName)
-                            .append(" (").append(empJobTitle).append(")\n");
-                }
-                employeeTextArea.setText(employeesData.toString());
-
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-
-            displayDialog.getContentPane().add(scrollPane);
-            displayDialog.pack();
-            displayDialog.setLocationRelativeTo(this);
-            displayDialog.setVisible(true);
-        });
-
-        buttonPanel.add(displayAllButton);
-
-        // Add the main panel to the dialog
-        getContentPane().add(mainPanel);
+        // Add the main panel and the display all button to the dialog
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(mainPanel, BorderLayout.CENTER);
+        getContentPane().add(displayAllButton, BorderLayout.SOUTH);
 
         pack();
-        setLocationRelativeTo(parent); // Center the dialog on the parent frame
+        setLocationRelativeTo(parent);
     }
 
     private void saveEmployeeData() {
-        String firstName = firstNameField.getText();
-        String lastName = lastNameField.getText();
+        // Get the employee data from the text fields
         String employeeNumber = employeeNumberField.getText();
+        String lastName = lastNameField.getText();
+        String firstName = firstNameField.getText();
+        String extension = extensionField.getText();
         String email = emailField.getText();
         String officeCode = officeCodeField.getText();
         String reportsTo = reportsToField.getText();
         String jobTitle = jobTitleField.getText();
 
-        System.out.println("Employee Data: " + firstName + " " + lastName + " (" + jobTitle + ")");
-    }
-<<<<<<< HEAD
-    
-	 // Database connection parameters
-	    String url = "jdbc:mysql://localhost:3306/classicmodels";
-	    String username = "student";
-	    String password = "student";
-        {
-	    
-	    // Establish the database connection
-	    try {
-	        Connection connection = DriverManager.getConnection(url, username, password);
-	        // Perform database operations here (e.g., saving or modifying the employee data)
-	        // ...
-	
-	        // Close the database connection
-	        connection.close();
-	    } catch (SQLException e) {
-	        e.printStackTrace();
+        try {
+            // Create a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Create a statement
+            Statement statement = connection.createStatement();
+
+            // Insert the employee data into the database
+            String sql = "INSERT INTO employees (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle) " +
+                         "VALUES ('" + employeeNumber + "', '" + lastName + "', '" + firstName + "',  '" + extension + "', '" + email + "', '" +
+                         officeCode + "', '" + reportsTo + "', '" + jobTitle + "')";
+
+            statement.executeUpdate(sql);
+
+            // Close the statement and connection
+            statement.close();
+            connection.close();
+
+            JOptionPane.showMessageDialog(this, "Employee data saved successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-	}
-    
-=======
+    }
 
->>>>>>> e1f7682093e9c6e1d26587eba40de385557dae47
+    private void updateEmployeeData() {
+        // Get the employee data from the text fields
+        String employeeNumber = employeeNumberField.getText();
+        String lastName = lastNameField.getText();
+        String firstName = firstNameField.getText();
+        String extension = extensionField.getText();
+        String email = emailField.getText();
+        String officeCode = officeCodeField.getText();
+        String reportsTo = reportsToField.getText();
+        String jobTitle = jobTitleField.getText();
+
+        try {
+            // Create a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Create a statement
+            Statement statement = connection.createStatement();
+
+            // Update the employee data in the database
+            String sql = "UPDATE employees SET lastName = '" + lastName + "', firstName = '" + firstName + "', extension = '" + extension +
+                         "', email = '" + email + "', officeCode = '" + officeCode + "', reportsTo = '" + reportsTo +
+                         "', jobTitle = '" + jobTitle + "' WHERE employeeNumber = '" + employeeNumber + "'";
+
+            statement.executeUpdate(sql);
+
+            // Close the statement and connection
+            statement.close();
+            connection.close();
+
+            JOptionPane.showMessageDialog(this, "Employee data updated successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayAllEmployees() {
+        try {
+            // Create a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Create a statement
+            Statement statement = connection.createStatement();
+
+            // Execute the query to retrieve all employees
+            String sql = "SELECT * FROM employees";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // Process the result set and display the employee data
+            StringBuilder output = new StringBuilder();
+            while (resultSet.next()) {
+                int employeeNumber = resultSet.getInt("employeeNumber");
+                String lastName = resultSet.getString("lastName");
+                String firstName = resultSet.getString("firstName");
+                String extension = resultSet.getString("extension");
+                String email = resultSet.getString("email");
+                String officeCode = resultSet.getString("officeCode");
+                String reportsTo = resultSet.getString("reportsTo");
+                String jobTitle = resultSet.getString("jobTitle");
+
+                output.append("Employee Number: ").append(employeeNumber)
+                      .append(", Last Name: ").append(lastName)
+                      .append(", First Name: ").append(firstName)
+                      .append(", Extension: ").append(extension)
+                      .append(", Email: ").append(email)
+                      .append(", Office Code: ").append(officeCode)
+                      .append(", Reports To: ").append(reportsTo)
+                      .append(", Job Title: ").append(jobTitle)
+                      .append("\n");
+            }
+
+            // Close the result set, statement, and connection
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+            // Display the employee data in a dialog
+            JOptionPane.showMessageDialog(this, output.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Employee Management System");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Create and show the dialog
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton openDialogButton = new JButton("Add/Modify Employee");
-        openDialogButton.addActionListener(e -> {
-            EmployeeFormDialog dialog = new EmployeeFormDialog(frame);
-            dialog.setVisible(true);
+            JButton openButton = new JButton("Open Employee Form");
+            openButton.addActionListener(e -> {
+                EmployeeFormDialog dialog = new EmployeeFormDialog(frame);
+                dialog.setVisible(true);
+            });
+
+            frame.getContentPane().add(openButton);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(openDialogButton);
-
-        frame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
     }
 }
